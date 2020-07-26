5c70d8d63a98eae2279a4cb66e79e6b276ebb949
Ashley <ashley@mfrederi.local>
2020-07-26 13:36:13 -0400

    deleted

--- random_pw_generator.py
+++
@@ -1,9 +0,0 @@
-#build a program that intakes some words from the user 
-# then generates a random password using those words. 
-import random 
-
-word = input("Enter a word you will remember (the longer the better!): ")
-new_list = list(word)
-random.shuffle(new_list)
-final_list = ''.join(new_list)
-print(final_list)
